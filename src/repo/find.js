9034a37bb10e32354ac440406eee8d3c101be1d9
Tobias de Groen <tdgroen@lias-software.nl>
2016-11-03 14:11:15 +0100

    refactor(repo): split up.

---
+++ src/repo/find.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const into = require('into');
+const chalk = require('chalk');
+const shelljs = require('shelljs');
+const {
+  isRepository
+} = require('./search');
+const fsts = require('../fstreesearch');
+
+exports.find = function(path, cb) {
+  fsts.find(
+    path,
+    (path, next) => isRepository(path, (err, res) => next(err, res.found)),
+    (err, results) => {
+      results.reverse();
+      if (results.length === 0) {
+        cb(new Error('No repository found!'));
+      } else {
+        cb(null, results[0]);
+      }
+    }
+  );
+};
--- src/repo/index.js
+++ src/repo/index.js
@@ -1,15 +1,32 @@
-const path = require('path');
-const fs = require('fs');
+#! /usr/bin/env node
 
-exports.isRepository = function(pathRep, cb) {
-  fs.stat(path.join(pathRep, '.git'), function(err, stats) {
-    if (err && err.code === 'ENOENT') return cb(null, { dir: pathRep, type: null, found: false });
-    if (err) return cb(err);
-
-    if (stats.isDirectory()) {
-      cb(err, { dir: pathRep, type: 'git', found: true });
-    } else {
-      cb(err, { dir: pathRep, type: null, found: false });
-    }
-  });
+exports.help = {
+  description: 'Run command from folder of current repository.'
 };
+
+if (require.main === module) {
+  const process = require('process');
+  const path = require('path');
+  const into = require('into');
+  const chalk = require('chalk');
+  const shelljs = require('shelljs');
+  const {
+    isRepository
+  } = require('./search');
+  const fsts = require('../fstreesearch');
+
+  fsts.find(
+    process.cwd(),
+    (path, next) => isRepository(path, (err, res) => next(err, res.found)),
+    (err, results) => {
+      results.reverse();
+      if (results.length === 0) {
+        console.log(chalk.red('No repository found.'));
+      } else {
+        const cmd = process.argv.slice(2).map(x => x.indexOf(' ') > -1 ? `"${x.replace('"', '\\"')}"` : x).join(' ');
+        shelljs.cd(results[0]);
+        shelljs.exec(cmd);
+      }
+    }
+  );
+}
