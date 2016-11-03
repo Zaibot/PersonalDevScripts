0db0c6e492e49223de12646013e40b477cbae7f2
Tobias de Groen <tdgroen@lias-software.nl>
2016-11-03 14:11:34 +0100

    feature(gst): initial commit.

---
+++ src/gst/index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+exports.help = {
+  description: 'Launches SourceTree for current repository.'
+};
+exports.defaultConfig = {
+  sourcetree: 'C:/Program Files (x86)/Atlassian/SourceTree/SourceTree.exe'
+};
+
+if (require.main === module) {
+  const config = require('../config');
+  const process = require('process');
+  const path = require('path');
+  const chalk = require('chalk');
+  const { find } = require('../repo/find');
+  const child_process = require('child_process');
+
+  config.readConfiguration(process.cwd(), function(err, config) {
+    find(process.cwd(), (err, pathRepository) => {
+      if (err) return console.log(chalk.red(err.message));
+      child_process.exec(`"${path.resolve(config.sourcetree)}" -f "${pathRepository}" log`, { shell: true }).unref();
+    })
+  });
+}
