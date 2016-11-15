c4cf44cdcc49beb8c88350caa92e671a3bbae638
Tobias de Groen <tdgroen@zaibot.net>
2016-11-15 20:03:29 +0100

    fix(chromium): launch without waiting.

--- src/chromium/index.js
+++ src/chromium/index.js
@@ -11,10 +11,11 @@ if (require.main === module) {
   const config = require('../config');
   const process = require('process');
   const path = require('path');
-  const shelljs = require('shelljs');
+  const child_process = require('child_process');
 
+  const args = process.argv.slice(2).map(x => x.indexOf(' ') > -1 ? `"${x.replace('"', '\\"')}"` : x).join(' ');
+  
   config.readConfiguration(process.cwd(), function(err, config) {
-    const args = process.argv.slice(2).map(x => x.indexOf(' ') > -1 ? `"${x.replace('"', '\\"')}"` : x).join(' ');
-    shelljs.exec(`"${path.resolve(config.chromium)}" ${args}`);
+    child_process.spawn(`"${path.resolve(config.chromium)}" ${args}`, { shell: true, detached: true, stdio: 'ignore' }).unref();
   });
 }
