153a4adb5a64a1900c923d02f51a22834346c8bb
Tobias de Groen <tdgroen@zaibot.net>
2017-05-12 17:14:15 +0200

    feature(glog): indenting

--- src/glog/index.js
+++ src/glog/index.js
@@ -1,14 +1,63 @@
 #! /usr/bin/env node --harmony-async-await
 
 exports.help = {
-    description: 'GIT Merged History.'
+  description: 'GIT Merged History.'
 };
 
 const Git = require("nodegit");
 const chalk = require('chalk');
 
+class Level {
+  constructor() {
+    this._depths = new Map();
+  }
+  adviceDown(child, parent) {
+    if (this._depths.get(parent)) {
+      return;
+    }
+    const childLevel = this._depths.get(child) || 0;
+    this._depths.set(parent, childLevel + 1);
+  }
+  adviceUp(child, parent) {
+    if (this._depths.get(parent)) {
+      return;
+    }
+    const childLevel = this._depths.get(child) || 0;
+    this._depths.set(parent, childLevel);
+  }
+  get(hash) {
+    return this._depths.get(hash) || 0;
+  }
+}
+
 function oneLine(message) {
   return message.split('\n')[0].trim();
+}
+
+function remainderLines(message) {
+  return message.split('\n').slice(1).map(x => x.trim()).filter(Boolean);
+}
+
+function indent(level, ch = '  ') {
+  var r = '';
+  while (level-- > 0) {
+    r += ch;
+  }
+  return r;
+}
+async function find(repo, text) {
+  try {
+    var p = Git.Oid.fromString(text);
+    if (!p.iszero()) {
+      return p;
+    }
+  } catch (ex) {
+    console.error(`[find] ${ex.toString()}`);
+  }
+  const ref = await repo.getReference(text);
+  const t = await ref.resolve();
+  const targ = await t.target();
+  return targ;
 }
 async function listReferences(repo) {
   const refs = (await repo.getReferenceNames(Git.Reference.TYPE.OID)).sort();
@@ -29,8 +78,8 @@ async function listReferences(repo) {
 }
 async function resolveRange(repo, start, end) {
   try {
-    const startCommit = start && await repo.getReferenceCommit(start);
-    const endCommit = end && await repo.getReferenceCommit(end);
+    const startCommit = start && await repo.getCommit(await find(repo, start));
+    const endCommit = end && await repo.getCommit(await find(repo, end));
     return {
       startCommit,
       endCommit
@@ -39,6 +88,28 @@ async function resolveRange(repo, start, end) {
     await listReferences(repo);
     throw ex;
   }
+}
+
+function printInfo(startCommit, endCommit) {
+  console.log(chalk.grey(`Changes from ${chalk.white(oneLine(startCommit.message()))} till ${chalk.white(endCommit ? oneLine(endCommit.message()) : 'start')}`));
+}
+
+function printGenericMergeCommits(depth, count) {
+  console.log(`${chalk.grey(indent(8, '~'))} ${indent(depth)}${chalk.yellow(`~~~ ${count} generic merge commits ~~~`)}`);
+}
+
+function printCommit(depth, sha, message) {
+  console.log(`${chalk.grey(sha.substr(0, 8))} ${indent(depth)}${chalk.cyan(oneLine(message))}`);
+  const lines = remainderLines(message);
+  if (lines.length) {
+    for (const line of lines) {
+      console.log(`${indent(8, ' ')} ${indent(depth)}${chalk.grey('+')} ${chalk.grey(line)}`);
+    }
+  }
+}
+
+function isGenericMergeCommit(message) {
+  return /^Merge branch/.test(message);
 }
 async function printHistory(start = 'HEAD', end) {
   try {
@@ -50,8 +121,16 @@ async function printHistory(start = 'HEAD', end) {
     const history = startCommit.history();
     let completed = false;
     const endCommitHash = endCommit && endCommit.sha();
-
-    console.log(chalk.grey(`Changes from ${chalk.white(oneLine(startCommit.message()))} till ${chalk.white(endCommit ? oneLine(endCommit.message()) : 'start')}`));
+    const depths = new Level();
+    let genericMergeCommits = 0;
+    printInfo(startCommit, endCommit);
+    history.on("end", async function(commit) {
+      if (genericMergeCommits > 0) {
+        // Report generic merge commits
+        printGenericMergeCommits(depths.get(sha), genericMergeCommits);
+        genericMergeCommits = 0;
+      }
+    });
     history.on("commit", async function(commit) {
       if (completed) {
         return;
@@ -63,27 +142,36 @@ async function printHistory(start = 'HEAD', end) {
       const sha = commit.sha();
       const message = commit.message();
       const parents = commit.parents();
-      if (parents.length === 1) { return; }
-      if (/^Merge branch/.test(message)) { return; }
-      console.log(`${chalk.grey(sha.substr(0, 8))} ${chalk.cyan(oneLine(commit.message()))}`);
+      if (parents[0]) {
+        depths.adviceUp(sha.toString(), parents[0].toString());
+      }
+      if (parents[1]) {
+        depths.adviceDown(sha.toString(), parents[1].toString());
+      }
+      if (parents.length === 1) {
+        return;
+      }
+      if (isGenericMergeCommit(message)) {
+        return genericMergeCommits++;
+      }
+      if (genericMergeCommits > 0) {
+        // Report generic merge commits
+        printGenericMergeCommits(depths.get(sha), genericMergeCommits);
+        genericMergeCommits = 0;
+      }
+      printCommit(depths.get(sha), sha, message);
     });
     history.start();
   } catch (err) {
     console.error(err);
   }
 }
-async function depth(commit, num = 0) {
-  const p = await commit.getParents(8);
-  if (p.length > 0) {
-    return await depth(p[p.length - 1], num + 1)
-  } else {
-    return num + 1;
-  }
-}
 
 function main() {
-  const [ start, end ] = process.argv.slice(2);
+  const [start, end] = process.argv.slice(2);
   printHistory(start, end);
 }
 
-main();
+if (require.main === module) {
+  main();
+}
