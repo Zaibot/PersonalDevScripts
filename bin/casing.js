9a47fc10ac989ecb7a843ffa5cacdc7026523708
Tobias de Groen <tdgroen@zaibot.net>
2017-09-18 14:44:14 +0200

    change(casing): check if folder exists on disk
    + progress

--- bin/casing.js
+++ bin/casing.js
@@ -9,51 +9,83 @@ if (require.main === module) {
 }
 
 async function main() {
-  const cache = new Map();
-  const getList = async (p) => {
-    if (cache.has(p)) {
-      return cache.get(p);
-    } else {
-      const l = await silent(`ls -a "${p}"`);
-      cache.set(p, l);
-      return l;
-    }
-  };
-  const fs = require('fs');
-  const path = require('path');
   const chalk = require('chalk');
-  const files = await silent(`git ls-files`);
-  console.log(chalk.cyanBright(`Correcting casings in GIT`));
-  for (const file of files) {
-    const folder = path.resolve(path.dirname(file));
-    const actual = (await getList(folder)).map((cur) => path.basename(cur));
-    const nameGit = path.basename(file);
-    const nameFs = actual.reduce((state, cur) => cur.toLowerCase() === nameGit.toLowerCase() ? cur : state, null);
-    if (nameFs && nameGit) {
-      if (nameFs !== nameGit) {
-        try {
-          console.log(chalk.yellowBright(`correcting ${path.join(folder, nameFs)}`));
-          await run(`git mv "${path.join(folder, nameGit)}" "${path.join(folder, nameFs)}"`);
-        } catch (ex) {
-          console.log(chalk.redBright(`failed ${file}: ${ex}`));
+  const ProgressBar = require('progress');
+  try {
+    const cache = new Map();
+    const getDirectoryContents = async (p, output) => {
+      if (cache.has(p)) {
+        return cache.get(p);
+      } else {
+        const isDir = await isDirectory(p);
+        if (!isDir) {
+          output.log(chalk.yellowBright(`Skipping ${p}`));
+        }
+        const l = isDir ? await silent(`ls -a "${p}"`) : [];
+        cache.set(p, l);
+        return l;
+      }
+    };
+    const fs = require('fs');
+    const path = require('path');
+    const files = await silent(`git ls-files`);
+    const bar = new ProgressBar(':percent :folder', { total: files.length });
+    output = {
+      error: (...args) => bar.interrupt(...args),
+      log: (...args) => bar.interrupt(...args),
+    };
+    output.log(chalk.cyanBright(`Correcting casings in GIT`));
+    for (const file of files) {
+      const folder = path.resolve(path.dirname(file));
+      const actual = (await getDirectoryContents(folder, output)).map((cur) => path.basename(cur));
+      const nameGit = path.basename(file);
+      const nameFs = actual.reduce((state, cur) => cur.toLowerCase() === nameGit.toLowerCase() ? cur : state, null);
+      if (nameFs && nameGit) {
+        if (nameFs !== nameGit) {
+          try {
+            output.log(chalk.yellowBright(`correcting ${path.join(folder, nameFs)}`));
+            await run(`git mv "${path.join(folder, nameGit)}" "${path.join(folder, nameFs)}"`, output);
+          } catch (ex) {
+            output.log(chalk.redBright(`failed ${file}: ${ex}`));
+          }
         }
       }
+      bar.tick({ folder: path.dirname(file) });
     }
+    console.log(chalk.cyanBright(`Done.`));
+  } catch (ex) {
+    console.error(chalk.redBright(ex.toString()));
   }
-  console.log(chalk.cyanBright(`Done.`));
 }
 
-function silent(cmd) {
+function isDirectory(path) {
+  const fs = require('fs');
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          resolve(false);
+        } else {
+          reject(err);
+        }
+      } else {
+        resolve(stats.isDirectory());
+      }
+    })
+  });
+}
+
+function silent(cmd, output) {
   const chalk = require('chalk');
   const child_process = require('child_process');
   return new Promise((resolve, reject) => {
     child_process.exec(cmd, { maxBuffer: 1024 * 1024 }, (err, stdout, stderr) => {
       if (err) {
-        console.error(chalk.redBright(cmd));
-        console.error(chalk.gray(err));
-        reject(Error(`error code ${err.code}`));
+        output.error(chalk.redBright(cmd));
+        output.error(chalk.gray(err));
+        reject(err);
       } else {
-        resolve(stdout.trim().split(/\r?\n/))
+        resolve(stdout.trim().split(/\r?\n/));
       }
     });
   });
